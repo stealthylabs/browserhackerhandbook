c259ee68b76ceb63282008efcd44a59670ad8ff9
Stealthy Labs <github@stealthylabs.com>
2016-08-12 20:07:40 +0000

    fixing the userhome.jsp example since Mojo's render function handles XSS

--- browservictim.pl
+++ browservictim.pl
@@ -4,23 +4,21 @@ use Mojo::Util qw(url_escape);
 
 get '/' => sub {
     my $c = shift;
-    my $refxss = url_escape '<iframe allowfullscreen=1 style="border:none;margin:0;width:100%;height:100%" src="http://www.stealthylabs.com">&nbsp;</iframe>';
+    my $refxss1 = url_escape '<iframe allowfullscreen=1 style="border:none;margin:0;width:100%;height:100%" src="http://www.stealthylabs.com">&nbsp;</iframe>';
+    my $refxss2 = url_escape q{<script type="text/javascript">alert('You are a browser victim!');</script>};
     $c->render(template => 'index',
-        refxss => "/userhome?user=$refxss",
+        refxss1 => "/userhome.jsp?user=$refxss1",
+        refxss2 => "/userhome.jsp?user=$refxss2",
     );
 } => 'index';
 
 get '/userhome.jsp' => sub {
-    shift->redirect_to('userhome');
-};
-
-get '/userhome' => sub {
     my $c = shift;
     my $userid = $c->param('user');
     $c->render(text => "Your user id is $userid");
-} => 'userhome';
+};
 
-    app->start;
+app->start;
 __DATA__
 
 @@ index.html.ep
@@ -29,14 +27,10 @@ __DATA__
 <h1>Welcome to the Browser Hacker Handbook practice framework!</h1>
 To learn more, you can read the book.
 <ul>
-    <li><a href="<%= $refxss %>">Reflected XSS by injecting iframe</a></li>
+    <li><a href="<%= $refxss1 %>">Reflected XSS by injecting iframe</a></li>
+    <li><a href="<%= $refxss2 %>">Reflected XSS by injecting javascript</a></li>
 </ul>
 
-
-@@ userhome.html.ep
-% layout 'default';
-% title 'User Home';
-Your user id is <%= $userid %>.
 
 @@ layouts/default.html.ep
 <!DOCTYPE html>
--- t/01_01_reflectedxss.t
+++ t/01_01_reflectedxss.t
@@ -9,10 +9,13 @@ require "$FindBin::Bin/../browservictim.pl";
 my $t = Test::Mojo->new;
 $t->get_ok('/')->status_is(200)->content_like(qr/Browser Hacker Handbook/);
 
-$t->get_ok('/userhome.jsp')->status_is(302);
+$t->get_ok($t->app->url_for('/userhome.jsp'), form => {
+        user => '<strong>browserhackerhandbook</strong>'})->status_is(200)
+        ->element_exists('strong')->content_like(qr/browserhackerhandbook/);
 
-my $rxss = url_escape '<strong>browserhackerhandbook</strong>';
-$t->get_ok("/userhome?user=$rxss")->status_is(200)->element_exists('strong');
+$t->get_ok($t->app->url_for('/userhome.jsp'), form => {
+        user => '<script>alert("browser victim");</script>'})->status_is(200)
+        ->element_exists('script')->content_like(qr/browser victim/);
 
 
 done_testing();
